91b740f4d0cc1938d1d8c8ae44a197723d942c1c
Jan Helcl <janhelcl@gmail.com>
2026-08-26 22:39:33 +0200

    refactor: route historical analysis through atmospheric contracts

--- src/schema/atmospheric-profile.ts
+++ src/schema/atmospheric-profile.ts
@@ -1,9 +1,15 @@
 import * as z from "zod/v4";
 import { gefsEnsembleProfileQuerySchema, gefsEnsembleProfileResultSchema } from "./gefs-ensemble-profile.js";
+import { historicalProfileQuerySchema } from "./history.js";
+import { historicalProfileResultSchema } from "./history-result.js";
 import { profileQuerySchema } from "./query.js";
 import { profileResultSchema } from "./result.js";
 
-export const atmosphericModelIdSchema = z.enum(["gfs_0p25", "gefs_0p50"]);
+export const atmosphericModelIdSchema = z.enum([
+  "gfs_0p25",
+  "gefs_0p50",
+  "gfs_grid4_analysis_0p5",
+]);
 
 export const atmosphericProfileRequestSchema = z.union([
   z.object({
@@ -14,11 +20,16 @@ export const atmosphericProfileRequestSchema = z.union([
     model: z.literal("gefs_0p50"),
     query: gefsEnsembleProfileQuerySchema,
   }),
+  z.object({
+    model: z.literal("gfs_grid4_analysis_0p5"),
+    query: historicalProfileQuerySchema,
+  }),
 ]);
 
 export const atmosphericProfileResultSchema = z.union([
   profileResultSchema,
   gefsEnsembleProfileResultSchema,
+  historicalProfileResultSchema,
 ]);
 
 export type AtmosphericProfileRequestInput = z.input<typeof atmosphericProfileRequestSchema>;
